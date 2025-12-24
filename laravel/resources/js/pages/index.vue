5a38fb1865829fc5c47af559a5dab5fe1072d6ef
ryotaKFC <ryotamokatoka@gmail.com>
2025-12-24 13:12:11 +0900

    feat: headerコンポーネントの作成

--- laravel/resources/js/pages/index.vue
+++ laravel/resources/js/pages/index.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
 import Welcome from "../features/Welcome.vue";
+import Header from "@/features/header/components/Header.vue"
 </script>
 
 <template>
+    <Header />
   <Welcome />
 </template>
