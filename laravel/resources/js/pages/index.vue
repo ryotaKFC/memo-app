7e13c7113d0542407e68804ea2c3974a890f2891
ryotaKFC <ryotamokatoka@gmail.com>
2025-12-24 16:31:28 +0900

    feat: メモ作成フォームの見た目完成

--- laravel/resources/js/pages/index.vue
+++ laravel/resources/js/pages/index.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
 import Welcome from "../features/Welcome.vue";
 import Header from "@/features/header/components/Header.vue"
+import CreateMemoForm from "@/features/memo/components/CreateMemoForm.vue";
 </script>
 
 <template>
     <Header />
-  <Welcome />
+    <main class="mx-auto px-6 py-8 max-w-2xl">
+        <CreateMemoForm />
+    </main>
 </template>
