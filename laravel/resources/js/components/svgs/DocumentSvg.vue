c6b60d441cec7a9511a109e1c3954d48903384d7
ryotaKFC <ryotamokatoka@gmail.com>
2025-12-30 15:42:52 +0900

    feat: 外からサイズ調整できるようにした

--- laravel/resources/js/components/svgs/DocumentSvg.vue
+++ laravel/resources/js/components/svgs/DocumentSvg.vue
@@ -1,8 +1,19 @@
+<script setup lang="ts">
+const props = withDefaults(
+  defineProps<{
+    size?: number | null;
+  }>(),
+  {
+    size: 32,
+  },
+);
+</script>
+
 <template>
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    width="32"
-    height="32"
+    :width="props.size"
+    :height="props.size"
     viewBox="0 0 24 24"
     fill="none"
     stroke="currentColor"
